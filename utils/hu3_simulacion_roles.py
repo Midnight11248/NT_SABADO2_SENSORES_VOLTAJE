f7e535c034b434fcb68c4b7ce0179b0371c86908
Midnight11248 <megah@tapi.re>
2026-04-20 18:05:18 -0500

    feat Actualiza simulacion de roles y main

--- utils/hu3_simulacion_roles.py
+++ utils/hu3_simulacion_roles.py
@@ -1,27 +1,33 @@
 import random
 from datetime import datetime, timedelta
 
-
 def simular_roles(numeroRoles):
-    
-    listaRoles = ["Administrador", "root", "SoloLectura"]
-    
-    codigoRoles = ["01", "02", "03"]
-    
-    roles=[]
-    
-    fechaInicial = datetime(2010, 1, 1)
+
+    lista_roles = [
+        {"nombre_rol": "root", "codigo": "01"},
+        {"nombre_rol": "Administrador", "codigo": "02"},
+        {"nombre_rol": "SoloLectura", "codigo": "03"}
+    ]
+
+    roles = []
+
     for i in range(numeroRoles):
-        fechaSimulada = fechaInicial + timedelta(days=random.randint(0,60))
+        rol_seleccionado = random.choice(lista_roles)
+
         rol = {
-            "id_roles":random.randint(0, 5000),
-            "Administrador": random.choice(listaRoles),
-            "SoloLectura": random.choice(listaRoles),
-            "codigo": random.choice(codigoRoles),           
-            "root": random.randint(0, 5000),
-            "valor": random.randint(80000, 100000),
-            "fecha": fechaSimulada.strftime("%Y/%m/%d")
+            "nombre_rol": i + 1,
+            "nombre_rol": rol_seleccionado["nombre_rol"],
+            "codigo": rol_seleccionado["codigo"],
         }
-        
+
         roles.append(rol)
-    return roles    
+
+    return roles
+# ejemplo de uso de la función
+
+# roles_generados = simular_roles(10)
+
+# for rol in roles_generados:
+#     print(rol)
+
+
