3c96571673b8406ae3cb090010c02cb449f6ee9c
3pxm <emm4nuelperezmonsalve@gmail.com>
2026-04-22 18:22:27 -0500

    HU3: simulacion y exportacion de datos para tabla mediciones

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import pandas as pd
 from utils.simulacion_mediciones import generar_simulacion
 from notebook.hu1_limpieza_mediciones import limpiar_datos
 from notebook.hu2_descripcion_mediciones import describir_datos
+from notebook.hu3_simulacion_exportacion_mediciones import exportar_datos
 
 simulaciones = generar_simulacion(100)
 simulaciones_ordenadas = pd.DataFrame(simulaciones)
 
 simulaciones_limpias = limpiar_datos(simulaciones_ordenadas)
 describir_datos(simulaciones_limpias)
+exportar_datos()
