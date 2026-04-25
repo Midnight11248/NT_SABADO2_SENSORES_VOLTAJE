b348eb256e7e018389b987b6083bb6e0cc915a7b
Maria Jose <aristizabalperezmariajose@gmail.com>
2026-04-25 14:09:05 -0500

    Se crea la simulacion de exploracion

---
+++ notebook/HU2_exploracion_user.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from utils.simulacion_usuarios import generar_simulacion
+
+def exportar_datos():
+    simulaciones = generar_simulacion(1000)
+    data_frame = pd.DataFrame(simulaciones)
+    data_frame.to_csv('simulacion_usuarios.csv', index=False)
+    data_frame.to_json('simulacion_usuarios.json', orient='records', indent=4)
+    print('Archivo CSV generado: simulacion_usuarios.csv')
+    print('Archivo JSON generado: simulacion_usuarios.json')
+    print(f'Total de registros: {len(data_frame)}')
+
+exportar_datos()
